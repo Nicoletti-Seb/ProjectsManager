f3b1630641cba7db8e45c0af33f3294050c5c72d
leopaul29 <leopaulmartin@hotmail.fr>
2017-01-23 14:48:06 +0100

    multiroomchat

--- node-server.js
+++ node-server.js
@@ -1,9 +1,14 @@
 var http = require('http');
-
+var Multiroom = require('./modules_server/multiroomChatServer');
 
 module.exports = function NodeServer(app, hostname, port) {
 	var server = null;
-
+	console.log("multiroom:", Multiroom);
+	function onConnection() {
+		// appel multiroom
+		Multiroom.initMultiRoomChat(server);
+		console.log("onConnection");
+	}
 
 	function onStart() {
 		// eslint-disable-next-line no-console
@@ -13,6 +18,7 @@ module.exports = function NodeServer(app, hostname, port) {
 	function start() {
 		server = http.createServer(app);
 		server.on('listening', onStart);
+		server.on('connect', onConnection);
 		server.listen(port);
 	}
 
