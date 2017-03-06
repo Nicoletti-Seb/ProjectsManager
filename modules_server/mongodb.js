7df496d27b3eb14e160a19aeff721cbfa09bdbec
leopaul29 <leopaulmartin@hotmail.fr>
2017-03-06 15:47:49 +0100

    debug bd request

--- modules_server/mongodb.js
+++ modules_server/mongodb.js
@@ -1,44 +1,59 @@
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://localhost:27017/test';
-
+var url = 'mongodb://localhost:27017/projectsmanager';
 
 module.exports = (function MongodbServerClass() {
-	var mongodb = null;
+    var mongodb = null;
 
-	function connect() {
-		if (mongodb) {
-			return Promise.resolve();
-		}
+    function connect() {
+        if (mongodb) {
+            return Promise.resolve();
+        }
 
-		return new Promise(function executor(resolve, reject) {
-			MongoClient.connect(url, function onConnect(err, db) {
-				if(err){
-					reject(err);
-					return;
-				}
-				mongodb = db;
-				resolve();
-			});
-		});
-	}
+        return new Promise(function executor(resolve, reject) {
+            MongoClient.connect(url, function onConnect(err, db) {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                mongodb = db;
+                resolve();
+            });
+        });
+    }
 
-	function close() {
-		db.close();
-		db = null;
-	}
+    function close() {
+        db.close();
+        db = null;
+    }
 
-	function addProject(project) {
+    function addProject(project) {
 
-	}
+    }
 
-	function addUser(user) {
+    function addUser(user) {
 
-	}
+    }
 
-	return {
-		connect: connect,
-		close: close,
-		addProject: addProject,
-		addUser: addUser
-	};
+    function getUsers(callback) {
+        var findDocuments = function(db, callback) {
+            // Get the documents collection
+            var collection = db.collection('users');
+            // Find some documents
+            collection.find({}).toArray(function(err, docs) {
+                console.log("Found the following records");
+                console.log(docs)
+                callback(err, docs);
+            });
+        }
+
+        return findDocuments;
+    }
+
+    return {
+        connect: connect,
+        close: close,
+        addProject: addProject,
+        addUser: addUser,
+        getUsers: getUsers
+    };
 })();
--- modules_server/session.js
+++ modules_server/session.js
@@ -1,51 +1,55 @@
 var ProjectService = require('./room_services/project-service');
-
+var mongoDB = require('./mongodb');
 var DataTest = require('./data-test');
 
 /*eslint-disable vars-on-top */
 exports.createSession = function createSession(server) {
-	var io = require('socket.io').listen(server);
-	var users = DataTest.users;
-	var projects = [];
+    var io = require('socket.io').listen(server);
+    mongoDB.connect();
+    var users = DataTest.users;
+    var projects = [];
 
 
-	(function initProjects() {
-		projects = DataTest.projects;
-		projects.forEach(function loopInitRoom(project) {
-			project.currentUsers = [];
-		});
-	})();
+    (function initProjects() {
+        projects = DataTest.projects;
+        projects.forEach(function loopInitRoom(project) {
+            project.currentUsers = [];
+        });
+    })();
 
-	function connect(login, password) {
-		for (var i = 0, user = null; user = users[i]; i++) {
-			if (user.login === login && user.password === password) {
-				return user;
-			}
-		}
+    function connect(login, password) {
+        mongoDB.getUsers(function callback(err, users) {
+            console.log("user", users);
+            for (var i = 0, user = null; user = users[i]; i++) {
+                console.log("user", user);
+                if (user.login === login && user.password === password) {
+                    return user;
+                }
+            }
+            return null;
+        });
+    }
 
-		return null;
-	}
+    io.sockets.on('connection', function onConnection(socket) {
+        socket.on('authentication', function onAuthentication(login, password) {
+            var user = connect(login, password);
+            if (!user) {
+                socket.emit('authenticate', { error: 'User not found...' });
+                return;
+            }
 
-	io.sockets.on('connection', function onConnection(socket) {
-		socket.on('authentication', function onAuthentication(login, password) {
-			var user = connect(login, password);
-			if (!user) {
-				socket.emit('authenticate', { error: 'User not found...' });
-				return;
-			}
+            socket.user = user;
+            socket.currentProject = null;
+            ProjectService.listen(io, socket, projects);
 
-			socket.user = user;
-			socket.currentProject = null;
-			ProjectService.listen(io, socket, projects);
-
-			//notify client
-			socket.emit('authenticate');
-		});
+            //notify client
+            socket.emit('authenticate');
+        });
 
 
-		socket.on('disconnect', function onDisconnect() {
-			socket.disconnect();
-		});
-	});
+        socket.on('disconnect', function onDisconnect() {
+            socket.disconnect();
+        });
+    });
 };
 /*eslint-enable vars-on-top */
